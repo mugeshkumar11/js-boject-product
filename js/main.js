6bb18dba62e81b921745b05cbc141e292c629885
mugeshkumar <kmugesh11199@gmail.com>
2022-10-10 18:18:09 +0530

    add new file

--- js/main.js
+++ js/main.js
@@ -1,29 +1,74 @@
-let btnEle = document.getElementById("btn");
-let addEle = document.getElementById("add");
-var empty = []
-var qt = 0;
-var products = [
-    { id: 1, Name: "tv", price: 40000, size: "40",qty:0 },
-    { id: 2, Name: "phone", price: 20000, Ram: "6gp",qty:0 },
-    { id: 3, Name: "fridge", price: 60000, color: "black",qty:0 },
-    { id: 4, Name: "washingmachine", price: 30000, color: "blue",qty:0 }
-]
+var empty = [];
+var products =
+  
+[   { id: 1, Name: "tv", price: 40000, size: "40" },
+    { id: 2, Name: "phone", price: 20000, Ram: "6gp"},
+    { id: 3, Name: "fridge", price: 60000, color: "black" },
+    { id: 4, Name: "washingmachine", price: 30000, color: "blue"}];
 
 
-function select() {
-    let valueEle = parseInt(prompt("give value"));
-    let findValue = products.find(function(item){
-        return item.id==valueEle
+var btnEle=document.getElementById("add");
+var btn2Ele=document.getElementById("incre");
+var btn3Ele=document.getElementById("decre");
+var btn4Ele=document.getElementById("del");
 
-    })
-    empty.push(findValue)
-    console.log(empty)
+
+btnEle.addEventListener("click",function(){
+    let value=parseInt(prompt("enter value"));
+if(value){
+
+    let result=products.find(function(order){
+    return order.id===value;
+})
+    result.qty=1;
+    empty.push(result);
+    console.log(empty);
 }
-addEle.addEventListener("click",function(){
-    qt+=1;
-    var total = empty.qty =qt
-    console.log(empty)
 })
-btnEle.addEventListener("click", function () {
-    select();
-})
+
+
+function increment(){
+    let a=parseInt(prompt("increment +"));
+
+    empty.map((order)=>{
+        if(order.id==a){
+
+           order.qty+=1;
+        }   
+    })
+    console.log(empty);
+}
+btn2Ele.addEventListener("click",increment);
+
+function decrement(){
+    let a=parseInt(prompt("decrement -"));
+
+    empty.map((order)=>{
+        if(order.id==a){
+            order.qty-=1;
+            console.log(empty);
+            empty.map((order)=>{
+                if(order.qty==0){
+                    empty.splice(0);
+                    console.log(empty);
+                }
+            })
+        }
+        
+    })
+}
+btn3Ele.addEventListener("click",decrement);
+
+function del(){
+let a=parseInt(prompt("delete your protect"));
+let find = empty.find(element => element.id === a);
+if(find){
+    let ind=empty.findIndex(mugesh => mugesh.id === a);
+    empty.splice(ind,1)
+}
+console.log(empty);
+}
+btn4Ele.addEventListener("click",del);
+
+
+
